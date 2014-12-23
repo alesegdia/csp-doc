ab3746311fc105a019a6af8321265e0c9738878f
Alejandro Seguí <alesegdia@gmail.com>
2014-12-23 22:53:17 +0100

    More folder refactoring.

--- build-tilevisor.sh
+++
@@ -1,4 +0,0 @@
-cd ./libgdx-tilemap-visor
-./gradlew desktop:dist
-cp desktop/build/libs/desktop-1.0.jar ../tilevisor-desktop.jar
-cd ..
---
+++ libgdx-tilemap-visor/build-tilevisor.sh
@@ -0,0 +1,2 @@
+./gradlew desktop:dist
+cp desktop/build/libs/desktop-1.0.jar ./tilevisor-desktop.jar
