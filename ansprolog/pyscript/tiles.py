e0d68204a13ba7dc7b97885e6fdd7c22740e7c5a
Alejandro Seguí <alesegdia@gmail.com>
2015-01-08 04:03:03 +0100

    Now several kind of figures with same id don't overlap. Python visor reworked a little for better figure differing.

--- ansprolog/pyscript/tiles.py
+++ ansprolog/pyscript/tiles.py
@@ -22,11 +22,28 @@ class DimTok:
         return "Dimension: " + str(self.val)
 
 class TileTok:
+    next_identifier = 0
+    identifier_map = dict()
+
+    @staticmethod
+    def getIdentifier(identifier):
+        if identifier in TileTok.identifier_map:
+            return TileTok.identifier_map[identifier]
+        else:
+            TileTok.identifier_map[identifier] = TileTok.next_identifier
+            TileTok.next_identifier = TileTok.next_identifier + 1
+            return TileTok.identifier_map[identifier]
+
     def __init__(self, args):
         gs = args.split(",")
+        gs, self.figtype = gs[1:], gs[0]
         self.identifier, self.x, self.y = [ int(x)-1 for x in gs ]
+        self.identifier = str(self.identifier) + self.figtype
+        self.identifier = TileTok.getIdentifier(self.identifier)
+
     def __str__(self):
         return "[" + str(self.identifier) + ": " + str(self.x) + "," + str(self.y) + "]"
+
 
 def build_map1(parsestr):
     data = build_data(parsestr)
